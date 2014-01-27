666566e4e488ee8bf864bab3a90961b3372bb98f
Ian Cordasco <ian@bendyworks.com>
2014-01-26 19:34:55 -0600

    Test CaseStudy.database_connection

--- spec/unit/case_study_spec.rb
+++ spec/unit/case_study_spec.rb
@@ -9,4 +9,19 @@ describe CaseStudy do
       expect(CaseStudy.database_config).to eq({})
     end
   end
+
+  describe '.database_connection' do
+    let(:config) do
+      {'database' => 'test'}
+    end
+
+    before do
+      allow(CaseStudy).to receive(:database_config).and_return(config)
+    end
+
+    it 'should create a connection to the database' do
+      expect(Sequel).to receive(:postgres).with('test')
+      CaseStudy.database_connection
+    end
+  end
 end
