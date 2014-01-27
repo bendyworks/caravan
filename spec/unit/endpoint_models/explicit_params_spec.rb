83330f7cc6bb8f4517ec9dd367134827363e5b3d
Ian Cordasco <ian@bendyworks.com>
2014-01-27 17:45:00 -0600

    Add failing spec for ExplicitParams

--- spec/unit/endpoint_models/explicit_params_spec.rb
+++ spec/unit/endpoint_models/explicit_params_spec.rb
@@ -2,8 +2,21 @@ require 'endpoint_models/explicit_params'
 
 module EndpointModels
   describe ExplicitParams do
-    describe '.allow_params' do
+    describe 'allow_params' do
+      before do
+        class TestClass
+          extend ExplicitParams
+
+          allow_params :test_param
+        end
+      end
+
       it 'allows but does not require parameters' do
+        expect { TestClass.new }.not_to raise_error
+      end
+
+      it 'creates an attribute on the class' do
+        expect { TestClass.new.test_param }.not_to raise_error
       end
     end
   end
