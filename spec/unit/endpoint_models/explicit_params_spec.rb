a88ab8f7593f85e062f51b6320d26e1a9b341746
Ian Cordasco <ian@bendyworks.com>
2014-01-28 08:51:35 -0600

    Fix tests
    - The before was opening the class and adding the params each time

--- spec/unit/endpoint_models/explicit_params_spec.rb
+++ spec/unit/endpoint_models/explicit_params_spec.rb
@@ -3,41 +3,45 @@ require 'endpoint_models/explicit_params'
 module EndpointModels
   describe ExplicitParams do
     describe 'allow_params' do
-      before do
-        class TestClass
-          extend ExplicitParams
+      class TestAllowedParams
+        extend ExplicitParams
 
-          allow_params :test_param
-        end
+        allow_params :test_param
       end
 
+      let(:test_class) { TestAllowedParams }
+
       it 'allows but does not require parameters' do
-        expect { TestClass.new }.not_to raise_error
+        expect { test_class.new }.not_to raise_error
       end
 
       it 'creates an attribute on the class' do
-        expect { TestClass.new.test_param }.not_to raise_error
+        expect { test_class.new.test_param }.not_to raise_error
       end
 
       it 'accepts a hash with the parameter as a key' do
-        expect { TestClass.new({test_param: 'test'}) }.not_to raise_error
+        expect { test_class.new({test_param: 'test'}) }.not_to raise_error
       end
 
       it 'saves the name of the attribute' do
-        expect(TestClass.allowed_parameters).to include(:test_param)
+        expect(test_class.allowed_parameters).to include(:test_param)
+      end
+
+      it 'sets the attribute with the passed in parameter' do
+        expect(test_class.new({test_param: 'test'}).test_param).to eq('test')
       end
 
       context 'when a default is provided' do
-        before do
-          class TestCase
-            extend ExplicitParams
+        class TestAllowedParamsWithDefaults
+          extend ExplicitParams
 
-            allow_params :test_param, default: true
-          end
+          allow_params :test_param, default: true
         end
+        
+        let(:test_class) { TestAllowedParamsWithDefaults }
 
         it 'returns the default if no parameter is specified' do
-          expect(TestClass.new.test_param).to be_true
+          expect(test_class.new.test_param).to be_true
         end
       end
     end
