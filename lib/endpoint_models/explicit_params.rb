69ad592cd1d3bcf6b75d30eeb2871972fb460c5a
Ian Cordasco <ian@bendyworks.com>
2014-01-27 18:04:29 -0600

    Create a module to hold the instance methods
    - ExplicitParams.extended performs the magic to include InstanceMethods

--- lib/endpoint_models/explicit_params.rb
+++ lib/endpoint_models/explicit_params.rb
@@ -16,5 +16,26 @@ module EndpointModels
     def required_parameters
       @required_parameters ||= []
     end
+
+  private
+
+    def self.extended(klass)
+      klass.class_eval do
+        # When a class extends a module, you cannot add instance methods.
+        # Instead you have to do a bit more work to include some instance
+        # methods common to each class.
+        include InstanceMethods
+      end
+    end
+
+    # Methods that are needed for each instance of a class extending
+    # ExplicitParams
+    module InstanceMethods
+      def initialize(params={})
+        params.each do |attr, value|
+          instance_variable_set(:"@#{attr}", value)
+        end
+      end
+    end
   end
 end
