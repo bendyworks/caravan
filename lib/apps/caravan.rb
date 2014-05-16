c36f40419e0d1089fffdd9a0522e66c3950380b9
Betsy Lorton and Ian Cordasco <dev+schbetsy+sigmavirus24@bendyworks.com>
2014-05-16 13:26:29 -0500

    Fix how we build our Rack app and use Interpol middleware

--- lib/apps/caravan.rb
+++ lib/apps/caravan.rb
@@ -2,19 +2,27 @@ require 'caravan'
 
 Caravan.database_connection
 
+require File.expand_path("../../../config/interpol", __FILE__)
+
 require 'apps/users'
 require 'rack/builder'
+require 'interpol/request_body_validator'
+require 'interpol/response_schema_validator'
+require 'interpol/sinatra/request_params_parser'
 
 module Apps
   # The master application which maps each sub-application to its root
   # For example, Apps::Users is mount under /users
   class Caravan
     def self.new
-      app = Rack::Builder.app do
-        map('/users') { run Apps::Users }
+      app = Rack::Builder.new do
+        use Interpol::RequestBodyValidator
+        use Interpol::ResponseSchemaValidator
+
+        map('/users') { run Apps::Users.new }
       end
 
-      Rack::Builder.new do
+      Rack::Builder.app do
         run app
       end
     end
